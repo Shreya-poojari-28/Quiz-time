583694d85d5c1e85294677a95cf661b82ec7be7e
Shreya-poojari-28 <shreyapoojari28@gmail.com>
2025-03-11 17:25:18 +0530

    --

---
+++ script.js
@@ -0,0 +1,35 @@
+const startingTime =  0.5;
+let time = startingTime * 60;
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+function updateCountdown() {
+    const minutes = Math.floor(time/60)
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes} : ${seconds}`
+
+    time--;
+
+    if (time < 0) {
+        clearInterval(updateCountdown);
+        countdownEl.innerHTML = '00:00';
+    }
+
+    const body = document.body;
+    const countdown = document.getElementById('countdown');
+
+    if ((time <= (startingTime * 60) / 2) && (time >= (startingTime * 60) / 6)) {
+        body.style.backgroundColor = '#E4E5C7';
+        countdown.style.backgroundColor = '#ccbf3a'
+        countdown.style.boxShadow = 'inset 0px -5px 0px #d1ca65'
+    } else if (time <= (startingTime * 60) / 6) {
+        body.style.backgroundColor = '#DBADAD';
+        countdown.style.backgroundColor = '#cc4038'
+        countdown.style.boxShadow = 'inset 0px -5px 0px #d16863'
+    }
+}
+
